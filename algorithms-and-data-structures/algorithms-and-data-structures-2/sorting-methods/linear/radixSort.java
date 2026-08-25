f3e893ea3ea23352977203397226cbadd06168c1
Felipe Cotignola <fsa398975@gmail.com>
2026-08-25 07:41:21 -0300

    att

---
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/sorting-methods/linear/radixSort.java
@@ -0,0 +1,15 @@
+
+/*
+n==number of elements
+d==number of digits
+k==number of possible values per digit
+
+best case: o(d*(n+k))
+medium case: o(d*(n+k))
+worst case: o(d*(n+k))
+
+doesnt use comparisons
+very eficient for numbers with a limited number of digits
+
+sorts several nummbers using their digits processing one digit at time
+*/
