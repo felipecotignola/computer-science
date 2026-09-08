baa60daca61019c88db6275012364c31495ae1ad
Felipe <fsa398975@gmail.com>
2026-09-08 11:37:10 -0300

    att

--- algorithms-and-data-structures/algorithms-and-data-structures-2/data-structures/linear-data-structures/queue/circularQueue.c
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/data-structures/linear-data-structures/queue/circularQueue.c
@@ -1,18 +1,21 @@
 	typedef struct{
 		int* array;
-		int capacidade,quantidade;
+		int inicio,pos,capacidade,quantidade;
 	}Fila;
 	void set(*Fila struct,int n){
 		(*struct).array=malloc(n*sizeof(int));
 		(*struct).capacidade=n;
 		(*struct).quantidade=0;
+		(*struct).pos=0;
+		(*struct).inicio=0;
 	}
 	void enqueue(*Fila struct,int n){
 		if((*struct).quantidade==(*struct).capacidade){
 			return;
 		}
-		(*struct).array[(*struct).quantidade]=n;
-		(*struct).quantidade=(*struct).quantidade+1%(*struct).capacidade;
+		(*struct).array[(*struct).pos]=n;
+		(*struct).pos=(*struct).pos+1%(*struct).capacidade;
+		(*struct).quantidade++;
 	}
 	int dequeue(*Fila struct){
 		if((*struct).quantidade==0){
