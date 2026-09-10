69450d2cc19a0314bdab444b495a71069915ff6b
Felipe Cotignola <fsa398975@gmail.com>
2026-09-10 12:29:21 -0300

    att

--- algorithms-and-data-structures/algorithms-and-data-structures-2/practical-assignments/pa-02/javaModelling/javaModelling.java
+++ algorithms-and-data-structures/algorithms-and-data-structures-2/practical-assignments/pa-02/javaModelling/javaModelling.java
@@ -1,88 +1,78 @@
-,class Data{
+class Data{
 	private int dia,mes,ano;
-	public Data(int dia,int mes,int ano){
-		this.dia=dia;
-		this.mes=mes;
+	public Data(int ano,int mes, int dia){
 		this.ano=ano;
+		this.mes=mes;
+		this.dia=dia;
 	}
-	public int getDia(){
-		return dia;
+	public static Data parseData(String s){
+		String[] atributos=s.Split('-');
+		int ano=Integer.parseInt(atributos[0]);
+		int mes=Integer.parseInt(atributos[1]);
+		int dia=Integer.parseInt(atributos[2]);
+		Data data=new Data(ano,mes,dia);		
+		return data;
 	}
-	public int getMes(){
-		return mes;
-	}
-	public int getAno(){
+	int getAno(){
 		return ano;
 	}
+	int getMes(){
+		return mes;
+	}
+	int getDia(){
+		return dia;
+	}	
 }
 class Veiculo{
-	private int id,ano,cilindros;
-	private float cilindradas,consumoCidade,consumoEstrada,co2;
-	private String marca,modelo,categoria,combustivel,transmissao,tracao;
-	private	boolean turbo;
+	private int id;
+	private String marca;
+	private String modelo;
+	private int ano;
+	private String categoria;
+	private String combustivel;
+	private int cilindros;
+	private float cilindrada;
+	private String transmissao;
+	private String tracao;
+	private float consumoCidade;
+	private float consumoEstrada;
+	private float co2;
+	private boolean turbo;
 	private Data data;
-	public Veiculo(int id,int ano,int cilindros,float cilindradas,float consumoCidade,float consumoEstrada,float co2,String marca,String modelo,String categoria,String combustivel,String transmissao,String tracao,boolean turbo,int dia,int mes,int ano2){
+	public Veiculo(int id,String marca,String modelo, int ano,String categoria,String combustivel,int cilindros,float cilindrada,String transmissao,String tracao,float consumoCidade,float consumoEstrada,float co2,boolean turbo,Data data){
 		this.id=id;
+		this.marca=marca;
+		this.modelo=modelo;
 		this.ano=ano;
-		this.cilindros=cilindros;
-		this.cilindradas=cilindradas;
+		this.categoria=categoria;
+		this.combustivel=combustivel;
+		this.cilindro=cilindro;
+		this.cilindrada=cilindrada;
+		this.transmissao=transmissao;
+		this.tracao=tracao;
 		this.consumoCidade=consumoCidade;
 		this.consumoEstrada=consumoEstrada;
 		this.co2=co2;
-		this.marca=marca;
-		this.modelo=modelo;
-		this.categoria=categoria;
-		this.combustivel=combustivel;
-		this.transmissao=transmissao;
-		this.tracao=tracao;
 		this.turbo=turbo;
-		this.data=new Data(dia,mes,ano2);	
+		this.data=data;	
 	}
-	public	int getId(){
-		return id;
-	}
-	public int getAno(){
-		return ano;
-	}
-	public int getCilindros(){
-		return cilindros;
-	}
-	public float getCilindradas(){
-		return cilindradas;
-	}
-	public float getConsumoCidade(){
-		return consumoCidade;
-	}
-	public float getConsumoEstrada(){
-		return consumoEstrada;
-	}
-	public float getCo2(){
-		return co2;
-	}
-	public String getMarca(){
-		return marca;
-	}
-	public String getModelo(){
-		return modelo;
-	}
-	public String getCategoria(){
-		return categoria;
-	}
-	public String getCombustivel(){
-		return combustivel;
-	}
-	public String getTransmissao(){
-		return transmissao;
-	}
-	public String getTracao(){
-		return tracao;
-	}
-	public boolean getTurbo(){
-		return turbo;
-	}
-	public Data getData(){
-		return data;
-	}
-	
-	
+	public static void parseVeiculo(String s){
+		String[] atributos= String.split(',');
+		int id=Integer.parseInt(atributos[0]);
+		String marca=atributos[1];
+		String modelo=atributos[2];
+		int ano=Integer.parseInt(atributos[3]);
+		String categoria=atributos[4];
+		String combustivel=atributos[5];
+		int cilindro=Integer.parseInt(atributos[6]);
+		float cilindradas=Float.parseFloat(atributos[7]);
+		String transmissao=atributos[8];
+		String tracao=atributos[9];
+		float consumoCidade=Float.parseFloat(atributos[10]);
+		float consumoEstrada=Float.parseFloat(atributos[11]);
+		float co2=Float.parseFloat(atributos[12]);
+		boolean turbo=Boolean.parseBoolean(atributos[13]);	
+		Data data=Data parseData(atributos[14]);
+			
+	}	
 }
